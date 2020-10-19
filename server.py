f603ccbe0c3567c18d11ba4129537872edd8cee5
temitope iyanuoluwa <temitope.akintilebo@deimos.co.za>
2020-10-19 03:43:14 +0100

    gunicorn and waitress

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import app
-
+import waitress
 
 if __name__ == "__main__":
     server = app.create_app()
-    server.run(host='0.0.0.0', port=5200, debug=True)
+    waitress.serve(app, host='0.0.0.0', port=5200,)
+    # server.run(host='0.0.0.0', port=5200, debug=True)
