9a9181e58536e835bf43f710de93a97f667bd29c
temitope iyanuoluwa <temitope.akintilebo@deimos.co.za>
2020-10-19 09:31:50 +0100

    fixed message

--- server.py
+++ server.py
@@ -3,5 +3,5 @@ import waitress
 
 if __name__ == "__main__":
     server = app.create_app()
-    waitress.serve(app, host='0.0.0.0', port=5200,)
+    waitress.serve(app.create_app(), host='0.0.0.0', port=5200)
     # server.run(host='0.0.0.0', port=5200, debug=True)
